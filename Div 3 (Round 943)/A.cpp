a0b418792a88815a3687333d98ccab80291ececc
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-05-03 16:23:56 +0600

    Add files via upload
    Codeforces Div 3 (Round 943)

---
+++ Div 3 (Round 943)/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t-->0)
+    {
+        int x;
+        cin >> x;
+
+        int y = x - 1;
+
+        cout << y << endl;
+    }
+    return 0;
+}
+
